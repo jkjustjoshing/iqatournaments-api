b221f6f1e0fd8f6c7f205fa49a0afc3a97df2d0e
Josh Kramer <josh@joshkra.me>
2014-01-08 00:47:34 -0500

    Allow various headers in request

--- app/app.js
+++ app/app.js
@@ -35,6 +35,7 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.all('*', function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
